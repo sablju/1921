5b97aed358ec6d92f88e6224940f95240b9ff292
Xuhui Shi <146591740+sablju@users.noreply.github.com>
2024-02-28 02:17:37 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 # 编译 maze.c 以生成可执行文件 maze
-gcc maze.c -o maze
-
-# 测试地图初始化功能
-echo "Testing map initialization:"
-if timeout 10s ./maze map.txt; then
-    echo "Map initialization: PASS"
-else
-    echo "Map initialization: FAIL"
-fi
-
+g++ maze.c -o maze
 
 # 测试地图显示功能
 echo -e "\nTesting map display:"
-if timeout 10s echo "M" | ./maze ; then
+if timeout 0.1s ./maze map.txt | echo "m" ; then
     echo "Map display: PASS"
 else
     echo "Map display: FAIL"
@@ -22,7 +13,7 @@ fi
 # 测试玩家移动功能
 echo -e "\nTesting player movement functionality:"
 # 运行 maze 程序，然后等待一段时间，终止进程
-if timeout 10s echo "ddd" | ./maze ; then
+if timeout 0.1s  ./maze map.txt | echo "ddd" ; then
     echo "Player movement functionality: PASS"
 else
     echo "Player movement functionality: FAIL"
@@ -31,7 +22,7 @@ fi
 # 测试无效输入
 echo -e "\nTesting invalid input:"
 # 运行 maze 程序，然后等待一段时间，终止进程
-if timeout 10s echo "43ez" | ./maze  > /dev/null; then
+if timeout 0.1s  ./maze map.txt|echo "43ez"  > /dev/null; then
     echo "Invalid input: PASS"
 else
     echo "Invalid input: FAIL"
@@ -40,7 +31,7 @@ fi
 # 测试到达终点功能
 echo -e "\nTesting reaching the end functionality:"
 # 运行 maze 程序，然后等待一段时间，终止进程
-if timeout 10s echo -e "dsssd" | ./maze ; then
+if timeout 0.1s ./maze map.txt |echo -e "dsssd"; then
     echo "Reaching the end functionality: PASS"
 else
     echo "Reaching the end functionality: FAIL"
@@ -49,7 +40,7 @@ fi
 # 测试无法移动到指定位置
 echo -e "\nTesting inability to move to specified position:"
 # 运行 maze 程序，然后等待一段时间，终止进程
-if timeout 10s echo -e "www" | ./maze > /dev/null; then
+if timeout 0.1s ./maze map.txt | echo -e "www" > /dev/null; then
     echo "Inability to move to specified position: PASS"
 else
     echo "Inability to move to specified position: FAIL"
